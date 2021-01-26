3a07f59d508340d35e582e187039b4f40d59fbab
dhamanea <56532004+dhamanea@users.noreply.github.com>
2021-01-26 11:33:57 -0700

    Update sensors.c

--- MicrocontrollerCode/Core/Src/usr/sensors.c
+++ MicrocontrollerCode/Core/Src/usr/sensors.c
@@ -4,5 +4,24 @@
  *  Created on: Jan 5, 2021
  *      Author: danieltakyi
  */
+#include "sensors.h"
 
+void CO2Conversion()
+{
+ 
+}
+void TempConversion()
+{
+  float tempVal;
+  
+   return tempVal = 0.001/(/*input pin read*/);   /*0.001 is voltage per degree celsius conversion value*/
+ }
+
+void NoiseConversion()
+{
+ float PinVoltage, dBVal;
+ 
+  PinVoltage = (/*input pin read*/) / 1024.0 * 5.0;   /* 5.0 is voltage that is powering sensor */
+  return  dBVal = PinVoltage * 50.0; /* 50.0 is voltage to dB conversion value */ 
+}
 
