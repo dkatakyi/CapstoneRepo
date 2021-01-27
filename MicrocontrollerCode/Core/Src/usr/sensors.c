a206a66ddb7421dbfee50e52335c07f17053ca6b
dhamanea <56532004+dhamanea@users.noreply.github.com>
2021-01-27 16:48:13 -0700

    Update sensors.c

--- MicrocontrollerCode/Core/Src/usr/sensors.c
+++ MicrocontrollerCode/Core/Src/usr/sensors.c
@@ -6,32 +6,36 @@
  */
 #include "sensors.h"
 
-uint32_t CO2Conversion()
+uint32_t CO2Conversion() 
 {
  if(/*input pin read*/ == 0)
  {
-  return 0;
+  return 0; /*this should print fault*/
  }
 
  else if((/*input pin read*/) < 0.4)  /*preheating voltage is from 0.2 to 0.4 */
  {
-  return -1;
+  return -1; /* this should print "preheating CO2"*/
  }
 
  else 
  {
-  int voltagedif = ((/*input pin read*/) * 1000) - 400;
+  int voltagedif = ((/*input pin read*/) * 1000) - 400; /*this is for voltage to PPM conversion*/
   float PPM = voltagedif * 50.0/16.0; 
   
   return PPM;
  }
- 
+ /*CO2 has a 100ms delay before looping*/
 }
+
+
 uint32_t TempConversion()
 {
   float tempVal;
   
    return tempVal = 0.001/(/*input pin read*/);   /*0.001 is voltage per degree celsius conversion value*/
+ 
+ /* has an unknown delay before looping*/
  }
 
 uint32_t NoiseConversion()
@@ -39,6 +43,9 @@ uint32_t NoiseConversion()
  float PinVoltage, dBVal;
  
   PinVoltage = (/*input pin read*/) / 1024.0 * 5.0;   /* 5.0 is voltage that is powering sensor */
-  return  dBVal = PinVoltage * 50.0; /* 50.0 is voltage to dB conversion value */ 
+  return  dBVal = PinVoltage * 50.0; /* 50.0 is voltage to dB conversion value */
+ 
+ 
+ /* has an 125ms delay before looping*/
 }
 
