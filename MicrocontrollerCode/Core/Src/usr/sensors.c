5b95e630d95d8f21dbbb3f0a2d06fb48c964f183
dkatakyi <dkatakyi@gmail.com>
2021-01-27 14:50:32 -0700

    Created ADC channel select functions. Created ADC read function.

--- MicrocontrollerCode/Core/Src/usr/sensors.c
+++ MicrocontrollerCode/Core/Src/usr/sensors.c
@@ -5,4 +5,60 @@
  *      Author: danieltakyi
  */
 
+#include "sensors.h"
+
+void CO2_Select(void)
+{
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel = ADC_CHANNEL_1;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+void TEMP_Select(void)
+{
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel = ADC_CHANNEL_4;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+void NOISE_Select(void)
+{
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel = ADC_CHANNEL_6;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+void POT_Select(void)
+{
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel = ADC_CHANNEL_7;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+uint32_t ADC_Read(void)
+{
+	uint32_t temp;
+	HAL_ADC_Start(&hadc1);
+	HAL_ADC_PollForConversion (&hadc1, 1000);
+	temp = HAL_ADC_GetValue(&hadc1);
+	HAL_ADC_Stop(&hadc1);
+	return temp;
+}
 
--- MicrocontrollerCode/Core/Src/usr/sensors.h
+++ MicrocontrollerCode/Core/Src/usr/sensors.h
@@ -8,6 +8,22 @@
 #ifndef SRC_USR_SENSORS_H_
 #define SRC_USR_SENSORS_H_
 
+#include <stdint.h>
+#include "main.h"
 
+
+extern ADC_HandleTypeDef hadc1;
+
+
+
+void CO2_Select(void);
+
+void TEMP_Select(void);
+
+void NOISE_Select(void);
+
+void POT_Select(void);
+
+uint32_t ADC_Read(void);
 
 #endif /* SRC_USR_SENSORS_H_ */
