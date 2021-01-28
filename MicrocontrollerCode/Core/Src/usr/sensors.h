8b8b95900608246bee205bb1beef861627c34ae3
dhamanea <56532004+dhamanea@users.noreply.github.com>
2021-01-27 17:29:42 -0700

    Update sensors.h

--- MicrocontrollerCode/Core/Src/usr/sensors.h
+++ MicrocontrollerCode/Core/Src/usr/sensors.h
@@ -8,8 +8,8 @@
 #ifndef SRC_USR_SENSORS_H_
 #define SRC_USR_SENSORS_H_
 
-uint32_t CO2Conversion();
-uint32_t TempConversion();
-uint32_t NoiseConversion();
+float CO2Conversion(uint32_t input);
+float TempConversion(uint32_t input);
+float NoiseConversion(uint32_t input);
 
 #endif /* SRC_USR_SENSORS_H_ */
