9183ab47145e2f3619571c1bd9bacb17ea229ed1
Aligwekwe Chiamaka <61888504+aligwekc@users.noreply.github.com>
2021-03-30 18:13:43 -0600

    updating file

--- RaspberryPi code/time_test.py
+++ RaspberryPi code/time_test.py
@@ -1,7 +1,9 @@
+#! /usr/bin/python3
 import subprocess
 from time import sleep
 
 y=(0.1)
-subprocess.Popen(["python",'file_save.py'])
+subprocess.Popen(["python3",'/home/pi/Desktop/file_save.py'])
 sleep(y)
-subprocess.Popen(["python",'ftptest.py'])
+subprocess.Popen(["python3",'/home/pi/Desktop/server_send.py'])
+sleep(y)
