d95da9b6a16c124c8bcb4c6f005063de2ff79f1d
Aligwekwe Chiamaka <61888504+aligwekc@users.noreply.github.com>
2021-03-30 18:14:07 -0600

    duplicate file

--- time_test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python3
-import subprocess
-from time import sleep
-
-y=(0.1)
-subprocess.Popen(["python3",'/home/pi/Desktop/file_save.py'])
-sleep(y)
-subprocess.Popen(["python3",'/home/pi/Desktop/server_send.py'])
-sleep(y)
