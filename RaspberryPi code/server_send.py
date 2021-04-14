811bccdad26ebab91a64624b84b0109785dc12e6
Aligwekwe Chiamaka <61888504+aligwekc@users.noreply.github.com>
2021-04-14 12:24:22 -0600

    updating server_send.py

--- RaspberryPi code/server_send.py
+++ RaspberryPi code/server_send.py
@@ -1,13 +1,17 @@
+#This program sends the csv file to the website via FTP
+#By Group 2 Capstone Project
 
+#!/usr/bin/python3
+
+#importing libaries 
 import ftplib
 import io
 import os
 from time import sleep
 
-#The csv file is sent to the FTP account to be displayed on the website.
 sleep(60) #initial set-up time
 while True:
-    sleep(60) #sent to the FTP every 1 min
+    sleep(60) #sent to the website every 1 min
     session = ftplib.FTP('192.168.1.67', 'ftpi', 'capstone')
     fp = open('/home/pi/Desktop/test/0b1.csv','rb')
     remotepath = '/home/ftpi/0b1.csv'
